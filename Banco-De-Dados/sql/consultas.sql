e588ca05169f254b3f4a2d56eb2f1775bd047ac2
Anderson Gabriel Barbosa <gaabrieelbarbosaa@gmail>
2025-03-25 10:55:41 -0300

    testando pastas vazias

--- Banco-De-Dados/sql/consultas.sql
+++ Banco-De-Dados/sql/consultas.sql
@@ -1,34 +1,11 @@
--- 1. Top 10 operadoras com maiores despesas hospitalares no último trimestre
-WITH dados_trimestre AS (
-    SELECT 
-        o.nome_fantasia,
-        o.uf,
-        o.cidade,
-        SUM(d.valor) AS total_despesas
-    FROM demonstracoes_contabeis d
-    JOIN operadoras o ON d.registro_ans = o.registro_ans
-    WHERE d.descricao LIKE '%EVENTOS/%SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR%'
-      AND d.competencia >= DATE_SUB(CURRENT_DATE, INTERVAL 3 MONTH)
-    GROUP BY o.nome_fantasia, o.uf, o.cidade
-    ORDER BY total_despesas DESC
-    LIMIT 10
+WITH ultimo_ano AS (
+    SELECT MAX(data) AS ultima_data FROM demonstrativos_contabeis
 )
-SELECT 
-    nome_fantasia AS "Operadora",
-    CONCAT(cidade, '/', uf) AS "Localização",
-    CONCAT('R$ ', FORMAT(total_despesas, 2, 'pt_BR')) AS "Despesas Trimestrais"
-FROM dados_trimestre;
-
--- 2. Top 10 operadoras com maiores despesas hospitalares no último ano
-SELECT 
-    o.nome_fantasia AS "Operadora",
-    o.telefone AS "Telefone",
-    o.endereco_eletronico AS "E-mail",
-    CONCAT('R$ ', FORMAT(SUM(d.valor), 2, 'pt_BR')) AS "Despesas Anuais"
-FROM demonstracoes_contabeis d
+SELECT o.nome_fantasia, SUM(d.final) AS total_despesas
+FROM demonstrativos_contabeis d
 JOIN operadoras o ON d.registro_ans = o.registro_ans
-WHERE d.descricao LIKE '%EVENTOS/%SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR%'
-  AND d.competencia >= DATE_SUB(CURRENT_DATE, INTERVAL 1 YEAR)
-GROUP BY o.nome_fantasia, o.telefone, o.endereco_eletronico
-ORDER BY SUM(d.valor) DESC
+WHERE d.descricao = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR'
+  AND d.data >= (SELECT ultima_data - INTERVAL '1 YEAR' FROM ultimo_ano)
+GROUP BY o.nome_fantasia
+ORDER BY total_despesas DESC
 LIMIT 10;
